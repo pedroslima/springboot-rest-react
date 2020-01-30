adb6472887a0c26be13428f5642da1c2a42de3be
Pedro Lima <pedro.lima@ncr.com>
2020-01-30 08:50:07 -0300

    controles hypermidia

--- src/main/java/com/plima/payroll/employee/EmployeeRepository.java
+++ src/main/java/com/plima/payroll/employee/EmployeeRepository.java
@@ -1,6 +1,7 @@
 package com.plima.payroll.employee;
 
-import org.springframework.data.repository.CrudRepository;
 
-public interface EmployeeRepository extends CrudRepository<Employee, Long> {
+import org.springframework.data.repository.PagingAndSortingRepository;
+
+public interface EmployeeRepository extends PagingAndSortingRepository<Employee, Long> {
 }
